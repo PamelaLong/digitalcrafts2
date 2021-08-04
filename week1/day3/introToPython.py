18e22cd2abf9579283b32b19fab02237b654b670
pamela long <usmc.pnlong@gmail.com>
2021-08-04 13:01:31 -0400

    Fingers crossed
    Sending the beginning part of the first assignment.

---
+++ week1/day3/introToPython.py
@@ -0,0 +1,12 @@
+firstName = "Pamela"
+lastName = "Long"
+fullName = firstName + lastName
+
+print(fullName)
+
+
+"Hello my name is Pamela Long"
+"I'm from Broadview, IL"
+"I'm excited to program"
+
+print("Hello my name is Pamela Long/I'm from Broadview, IL/I'm excited to program")
