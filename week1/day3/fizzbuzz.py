f773e34fda571eea4c3e81ff0d3556b7c3ed55e8
pamela long <usmc.pnlong@gmail.com>
2021-08-04 22:12:40 -0400

    Is it over yet? j/k

---
+++ week1/day3/fizzbuzz.py
@@ -0,0 +1,9 @@
+## Don't be mean to me
+
+number = int(input("Choose your number."))
+if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+elif number % 3 == 0:
+    print("Fizz")
+elif number % 5 == 0:
+    print("Buzz")
