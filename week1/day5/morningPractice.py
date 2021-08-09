160e8471c228efeb777646cd9e56a33a0bd2dae8
pamela long <usmc.pnlong@gmail.com>
2021-08-09 00:13:45 -0400

    day5

---
+++ week1/day5/morningPractice.py
@@ -0,0 +1,36 @@
+## Exercise 1
+# Avengers Assemble
+
+#ogAvengers = ["Captain America" , "Thor" , "Iron Man" , "Black Widow" , "Hulk"]
+#ogAvengers.append("Hawkeye")
+#print(ogAvengers)
+
+#del(ogAvengers) [2]
+#print(ogAvengers)
+
+#ogAvengers.insert(1, "Spider Man")
+#print(ogAvengers)
+
+## Exercise 2
+# Don't forget base zero
+
+#for b in range(1, 101):
+#    if b % 3 == 0 and b % 5 ==0:
+#        print("Fizz Buzz")
+#    elif(b % 3) == 0:
+#        print("Fizz")
+#    elif(b % 5) == 0:
+#        print("Buzz")
+#    else:
+#        print(b)
+
+## Exercise 3
+# An array is a list(JavaScript)
+
+array1 = [0, 1, 0, 3, 12]
+array2 = [1, 7, 0, 0, 8, 0, 10, 12, 0, 4]
+
+
+
+#output1: 1, 3, 12, 0, 0
+#output2: 1, 7, 8, 10, 12, 4, 0, 0, 0, 0
