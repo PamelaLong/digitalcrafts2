9c77184ad67f6f24df4597c40f2630e6de984a1e
pamela long <usmc.pnlong@gmail.com>
2021-08-09 00:14:06 -0400

    day4

---
+++ week1/day4/classassignment.py
@@ -0,0 +1,22 @@
+# Sum the Numbers
+
+# numbers_list = [2, 4, 6, 8, 10]
+# print(sum(numbers_list))
+
+
+# Largest Number
+
+# number2_list = [1, 2, 3, 4, 5]
+# print(len(number2_list))
+
+
+# Smaller Number
+
+# number3_list = [1, 3, 5, 7, 9]
+# print(min(number3_list))
+
+# Even Numbers
+
+number3_list = [1, 2, 3, 4, 5]
+print((number3_list))
+
