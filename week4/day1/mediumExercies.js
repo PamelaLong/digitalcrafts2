71c47b459631b080d4d20d00e1f32379f4dab220
pamela long <usmc.pnlong@gmail.com>
2021-08-23 17:27:01 -0400

    javascript exercises

---
+++ week4/day1/mediumExercies.js
@@ -0,0 +1,25 @@
+//Madlib
+
+
+// function madlib(name, subject) {
+//     return(`${name}'s favorite subject in school is ${subject}`);
+// }
+// console.log (madlib("Pamela", "math"));
+
+//Tip Calculator
+
+
+function tipAmount(billAmount, levelOfService) {
+    For (levelOfService) = [good, fair, poor];
+
+    if (levelOfService) [good, {
+        return(`${billAmount} * 20/100, ${levelOfService}`)
+    }
+    else if (levelOfService) fair, {
+        return(`${billAmount} * 15/100, ${levelOfService}`)
+    }
+    else if (levelOfServic) poor, {
+        return(`${billAmount} * 10/100, ${levelOfService}`)
+    }]
+}
+console.log (tipAmount ("100", "good"))
