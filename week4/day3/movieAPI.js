52be3bb65fbefb75ca5bca11a371b99a023f645e
pamela long <usmc.pnlong@gmail.com>
2021-08-30 14:47:16 -0400

    update

--- week4/day3/movieAPI.js
+++ week4/day3/movieAPI.js
@@ -1,24 +1,30 @@
-const myAPIKey = "d032f16a";
-const ombdPage = `http://img.omdbapi.com/?apikey=${myAPIKey}`;
+const myAPIKey = "d032f16a&s=";
+const ombdPage = `http://www.omdbapi.com/?apikey=${myAPIKey}`;
 
-const container = document.querySelector(".moviePosters");
+const input = document.querySelector(".input");
 const search = document.querySelector(".search");
 
 async function searchMovies(){
-    const movieContainer = document.querySelector(".input").value;
-    const fetchMovies = await fetch(ombdPage + `${input}`);
+    const userInput = input.value;
+    const fetchMovies = await fetch(ombdPage + `${userInput}`);
     const jsonMovies = await fetchMovies.json();
+    console.log(jsonMovies);
 
 
-for (const movie of jsonMovies.search) {
-    const movieContainer = document.createElement("div");
-    const moviePosters = document.createElement("img");
-    console.log(moviePosters)
+    for (const movie of jsonMovies.Search) {
+        // select a div where movies go inside of
+        const movieContainer = document.querySelector(".movieContainer");
+        // create div for images to go in to show on page
+        const divForMovies = document.createElement("div");
+        // this created image to go inside the div box
+        const movieImage = document.createElement("img");
+        // movieImage.innerHTML = ("");
+        movieImage.src = movie.Poster;
+        // I need to make the movie poster show up in divForMovies
+        divForMovies.append(movieImage);
+        // now that images are in the divForMovie box, you need to put it in the main container
+        movieContainer.append(divForMovies);
 
-    movieContainer.append(movie);
-    movie.append(movieTitle, moviePosters);
+    }
 }
-}
-search.addEventListener("click", (search) => {
-
-});
+search.addEventListener("click", searchMovies)
