3bd69d4c7de9683b6fa12096f7870e50a4da48db
pamela long <usmc.pnlong@gmail.com>
2021-08-29 22:21:08 -0400

    movie api

--- week4/day3/movieAPI.js
+++ week4/day3/movieAPI.js
@@ -1,14 +1,25 @@
 const myAPIKey = "d032f16a";
-const ombdPage = "http://www.omdbapi.com/?apikey=[d032f16a]";
+const ombdPage = `http://www.omdbapi.com/?apikey=${myAPIKey}`;
 
-const container = document.querySelector(".movieContainer");
-const search = document.querySelector(".search movies");
+const container = document.querySelector(".moviePosters");
+const search = document.querySelector(".search");
 
 async function searchMovies(){
-    const movieContainer = document.querySelector(".input");
-    const fetchMovies = await fetch(ombdPage);
-    const json = await fetch(fetchMovies.json);
-    const movieposts = document.createElement(".img");
+    const movieContainer = document.querySelector(".input").value;
+    const fetchMovies = await fetch(ombdPage + `${input}`);
+    const jsonMovies = await fetchMovies.json();
 
-console.log(ombdPage.search);
+
+for (const movie of jsonMovies.search) {
+    const moviePosters = document.createElement("img");
+    console.log(moviePosters)
+    const movieTitle = document.createElement("h2")
+    moviePosters.src = movie.moviePosters;
+    movieTitle.innerText = movieTitle;
+    movieContainer.append(moviePosters);
+    movie.append(movieTitle, moviePosters);
+    movieContainer.append(movie);
+
+    
+}
 }
