6d8026241317514995e99427e921f9ae620d2120
pamela long <usmc.pnlong@gmail.com>
2021-09-08 09:48:26 -0400

    assignment

--- week6/day1/index.js
+++ week6/day1/index.js
@@ -1,42 +1,58 @@
-const cowsay = require("cowsay");
-const express = require("express");
+// const cowsay = require("cowsay");
+// const express = require("express");
 
-// invoking the function
-const app = express();
+// // invoking the function
+// const app = express();
 
-//first one:
-// app.put("/hello world", (req, res) =>{
-//     res.send((console.log("Hello world")));
+// // //first route: hello world
+// app.get("/helloWorld", (req, res) => {
+//     res.send((console.log("/helloWorld")));
 // })
-//first one part 2:
-app.put("<h1>Hello world</h1>", (req, res) => {
-    console.log(req);
-    res.send("<h1>Hello world");
-});
+// app.listen(3000, console.log("Hello World!"));
+// console.log(
+//     cowsay.think({
+//     text: "Hello World!",
+//     e: "oO",
+//     T: "U",
+// }))
+//second route:
+// const cowsay = require("cowsay");
+// const express = require("express");
 
-// // console.log(
-// //     cowsay.think({
-// //     text: "I'm a mooodlue",
-// //     e: "oO",
-// //     T: "U",
-// // }))
+// const app = express();
 
-app.listen(3000, console.log("Hello world"));
-
-//second one:
 // app.get("/cats",(req, res) => {
 //     res.send((console.log("/cats")));
 // })
 // app.listen(3000, console.log("Meow"));
 
+
 //third one:
+// const cowsay = require("cowsay");
+// const express = require("express");
+
+// const app = express();
+
 // app.post("/dogs",(req, res) => {
 //     res.send((console.log("/dogs")));
 // })
 // app.listen(3000, console.log("Woof"));
 
 //forth one:
+// const cowsay = require("cowsay");
+// const express = require("express");
+
+// const app = express();
+
 // app.post("/cats_and_dogs",(req, res) => {
 //     res.send((console.log("/cats_and_dogs")));
 // })
 // app.listen(3000, console.log("Dogs and cats living together...mass hysteria!!"));
+
+const express = require("express");
+const app = express();
+
+app.get("/greet/:${Name}", (req, res) => {
+    res.send(console.log(`greet, ${Name}!`))
+})
+app.listen(5500, console.log("Hello, ${Name}!"));
