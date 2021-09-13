bea23a81267f0c1c328f587b17485721f6f207c8
pamela long <usmc.pnlong@gmail.com>
2021-09-13 09:46:44 -0400

    day 4 practice

---
+++ week6/day4/algorithm.js
@@ -0,0 +1,58 @@
+//#1
+// Given a string with a date "09/10/2021", write a function that removes the "/" and returns the following date format:
+// expected result: 20210910
+
+const str = "09/10/2021";
+
+function formatDate(mm,dd,yyyy) {
+    print("yyyymmdd")
+//write function body here
+    };
+// #2
+// Write a function that checks if a string is a palindrome. Function should return true or false. A palindrome is a word, phrase, or sequence that reads the same backward as forward.
+// Ex. racecar, Anna
+
+const str1 = "kayak"
+const str2 = "backendisawesome"
+const str3 = "mom"
+
+function checkString(){
+//write function body here
+}
+
+// #3
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+// Example
+// The minimum sum is  and the maximum sum is . The function prints
+
+// 16 24
+// Function Description
+// Complete the miniMaxSum function in the editor below.
+// miniMaxSum has the following parameter(s):
+
+// arr: an array of  integers
+// Print
+// Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+// Input Format
+// A single line of five space-separated integers.
+// Constraints
+// Output Format
+// Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+// Sample Input
+
+// 1 2 3 4 5
+// Sample Output:
+
+// 10 14
+
+//Anagrams
+
+// Write a function that takes in an array of strings and groups anagrams together.
+// Sample Input
+// [“yo”, “act”, “flop”, “tac”, “foo”, “cat”, “oy”, “olfp”]
+// Sample Output
+// [[“yo”, “oy”], [“flop”, “olfp”], [“act”, “tac”, “cat”], [“foo”]]
+function anagrams(array) {
+    let anagrams = ([yo, act, flop, tac, foo, cat, oy, olfp]);
+    
+};
