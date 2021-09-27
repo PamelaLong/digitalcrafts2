fb3aaf76e17a520ee8d3d5f17533cf05cdda2dec
pamela long <usmc.pnlong@gmail.com>
2021-09-27 16:25:48 -0400

    algo

---
+++ week7/day5/algorithm.js
@@ -0,0 +1,34 @@
+// const PORT = 3000;
+// const express = require("express");
+// const app = express;
+
+
+const maxStr1 = 'holddogecoinkek';
+const maxStr2 = 'aahhanotheralgo';
+const maxStr3 = 'randywhyyyyy';
+
+function maxStr1(holddogecoinkek){
+    var maxStr1 = 0;
+    maxChar = '';
+    str.split('').forEach(maxstr1(character));{
+        if(maxstr1.split(character).length > max){
+            max = str.split(character).length;
+            maxChar = character;
+        };
+    };
+    return maxChar;
+};
+console.log(maxStr1);
+
+
+// function maxStr2(aahhanotheralgo){
+//     return maxStr2;
+// }
+// console.log(maxStr2);
+
+
+// // function maxStr3(randywhyyyyy){
+// //     return maxStr3;
+// }
+// console.log(maxStr3);
+
