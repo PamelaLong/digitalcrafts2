6220ea5e94c01b2fec1e29ca644a9c5927019a79
pamela long <usmc.pnlong@gmail.com>
2021-10-05 14:31:57 -0400

    props drilling

--- week10/day2/props-drilling/src/App.js
+++ week10/day2/props-drilling/src/App.js
@@ -2,14 +2,18 @@
 import './App.css';
 import Dealership from './components/dealership/Dealership';
 import Neighborhood from './components/neighborhood/Neighborhood';
+import {useState} from "react";
 
 function App() {
-  const mustang = "1967 Mustang Boss 302"
+  const mustang = useState("1967 Mustang Boss 302");
+  const fordGT = "2021 Ford GT";
+
   return (
     <div className="box">
       <h1>props drilling</h1>
-      <Neighborhood />
-      <Dealership mustang={mustang}/>
+      {mustang}
+      <Neighborhood mustang={mustang} />
+      <Dealership mustang={mustang} fordGT={fordGT} />
 
     </div>
   );
--- week10/day2/props-drilling/src/index.js
+++ week10/day2/props-drilling/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
 
-
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, reduxDevTools);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap our app in our provider */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
