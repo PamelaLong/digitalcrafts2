eb49792475c8c1c798475aa0ac5b5f25f58787be
pamela long <usmc.pnlong@gmail.com>
2021-10-07 17:41:08 -0400

    redux practice not complete

---
+++ week10/day4/redux-day-3/src/components/SignUpForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+// import "./SignUpStyles.css";
+import { useDispatch } from 'react-redux';
+
+
+export default function SignUpForm() {
+    const dispatch = useDispatch();
+    const user  = useSelector((state) => state)
+
+    return ((
+      <div className="signUpForm">
+            <form>
+            <input onChange={(e) => dispatch({type: "SIGNUPFORM_FIRSTNAME", payload: e.target.value})} type="text" placeholder ="FirstName" />
+            <input onChange={(e) => dispatch({type: "SIGNUPFORM_LASTNAME", payload:e.target.value})} type="text" placeholder ="LastName" />
+            <input onChange={(e) => dispatch({type: "SIGNUPFORM_EMAIL", payload:e.target.value})} type="text" placeholder ="Email" />
+            <input onChange={(e) => dispatch({type: "SIGNUPFORM_PASSWORD", payload:e.target.value})} type="text" placeholder ="Password" />
+            <input type="submit" onClick={(e) => creatUserFromLogin(e, user)} />
+            </form>
+        </div>
+    
+    )
+    )
+};
---
+++ week10/day4/redux-day-3/src/reducers/rootReducer.js
@@ -0,0 +1,27 @@
+const initialState ={
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+
+};
+
+function rootReducer(state = initialState, action) {
+    switch (action.type){
+        case "SIGNUPFORM_FIRSTNAME":
+            return { ...state, firstName: action.payload };
+
+        case "SIGNUPFORM_LASTNAME":
+            return { ...state, firstName: action.payload };
+
+        case "SIGNUPFORM_EMAIL":
+            return { ...state, firstName: action.payload };
+
+        case "SIGNUPFORM_PASSWORD":
+            return { ...state, firstName: action.payload };
+        
+        default: 
+        return state;
+    }
+}
+export default rootReducer;
