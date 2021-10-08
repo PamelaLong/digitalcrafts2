922f2298bea695d779c33ba9daf911bd0ef775ed
pamela long <usmc.pnlong@gmail.com>
2021-10-08 13:14:25 -0400

    incomplete algo practice

---
+++ week9/day5/Justice/algoLeague.js
@@ -0,0 +1,20 @@
+[
+    {
+    name: "spiderman",
+    league: "otherHeroes",
+   },
+    {
+    name: "batman",
+    league: "Justice league",
+   }
+]
+
+const justiceLeague = [
+    "batman", "wonderwoman", "cyborg", "flash", "aquaman","spiderman", "superman", "greenlantern"
+]
+
+const theOtherHeroes = [
+    "crimsonmechdude", "hammerboy", "greenguy", "hawkguy", "blackassassin","shieldbro", "captneyepatch"
+]
+//1: console.log(theOtherHeroes[6]);
+//2:
