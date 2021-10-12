a5516e71fa9a071b5aaf0f37afcce707bab4d7e6
pamela long <usmc.pnlong@gmail.com>
2021-10-12 15:37:12 -0400

    REACT props drilling dealership

---
+++ week10/day2/props-drilling-cars/src/components/dealership/Dealership.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../../App.css";
+import { useSelector, useDispatch } from 'react-redux';
+
+export default function Dealership(props) {
+    const bugatti = useSelector(state =>state.bugatti)
+    const kawasaki = useSelector(state =>state.kawasaki)
+    const dispatch = useDispatch()
+
+    return (
+        <div className="box">
+            <h1>Dealeship</h1>
+            <h2>Cars in our dealership</h2>
+            <p>{props.mustang}</p>
+            <p>{props.fordGT}</p>
+            <p>{bugatti}</p>
+            <p>{kawasaki}</p>
+            <button onClick={()=> dispatch({type: "CHANGE_KAWASAKI"})}>Change Kawasaki</button>
+        </div>
+    )
+}
---
+++ week10/day2/props-drilling-cars/src/components/house/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Garage from '../garage/Garage'
+
+export default function Home() {
+    return (
+        <div>
+            <Garage />
+            <p></p>
+        </div>
+    )
+}
