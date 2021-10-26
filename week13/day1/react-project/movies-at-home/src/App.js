19d25e6c774a2ef7f37c720f0280ec35a70cdf96
pamela long <usmc.pnlong@gmail.com>
2021-10-26 16:08:35 -0400

    update commit

--- week13/day1/react-project/movies-at-home/src/App.js
+++ week13/day1/react-project/movies-at-home/src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
 import { useState } from 'react';
-import { useDispatch, useSelector } from "react-redux";
-// import movieTitle from './components/movieTitle';
-// import MovieContainer from './components/MovieContainer';
+import { useDispatch } from "react-redux";
 import SignUp from "./components/SignUp";
-import MovieContainer from './components/MovieContainer';
-
-
+// import Movies from './components/Movies';
 
 
 function App() {
   const inputValue = useState(0);
   const dispatch = useDispatch();
   const [SignUpItem, setSignUpItem] = useState({});
-  const [Movies, setMovies] = useSelector((state) => state.movie);
+  // const [MoviesTitle, setMoviesTitle] = useSelector({});
 
   return (
     <div className= "App">
       <h1>Sign Up To Order Movies</h1>
 
-        <MovieContainer Movies={Movies}
-        setMovies={setMovies}/>
+        {/* <Movies MoviesTitle={MoviesTitle}
+        setMoviesTitle={setMoviesTitle}/> */}
 
       <SignUp SignUpItem={SignUpItem}
       setSignUpItem={setSignUpItem}/>
--- week13/day1/react-project/movies-at-home/src/components/Movies.js
+++ week13/day1/react-project/movies-at-home/src/components/Movies.js
@@ -1,21 +1,32 @@
 // import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 // const myAPIKey = "aa7e3de3";
-// const omdbPage = `https://www.omdbapi.com/?i=tt3896198&apikey=aa7e3de3`;
-// const input = "userInput";
-// const jsonMovies = "jsonMovies";
+const omdbPage = `https://www.omdbapi.com/?i=tt3896198&apikey=aa7e3de3`;
+const input = "userInput";
 
 
 
 
+// const searchMovies = useSelector((state) => state.movies);
 
-export default function Movies(props) {
+export default async function Movies(props) {
 
-    const Movies = useSelector((state) => state.movies);
-    // const Poster = [];
+        const userInput = input.value;
+        const fetchMovies = await fetch(omdbPage + `${userInput}`);
+        const jsonMovies = await fetchMovies.json();
+        // console.log(jsonMovies);
+
+    for (const movie of jsonMovies.Search) {
+        const movieContainer = document.querySelector(".movieContainer");
+        const divForMovies = document.createElement("div");
+        const movieImage = document.createElement("img");
+        movieImage.src = movie.Poster;
+        divForMovies.append(movieImage);
+        movieContainer.append(divForMovies);
     return (
         <div>
-            <h1>{props?.movies}</h1>
+            <h1>{props?.movies.title}</h1>
         </div>
     )
+    }
 };
